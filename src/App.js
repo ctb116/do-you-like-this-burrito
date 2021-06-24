cada79ec34bd21f06bd78b0cec9c1183dae0478a
Cathy T Bradley <cathytbradley116@gmail.com>
2021-06-24 13:41:14 -0700

    test firebase access - all good

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useFirestore, useFirestoreDocData } from 'reactfire';
+
+function Burrito() {
+  const burritoRef = useFirestore()
+  .collection('foodTruck')
+  .doc('burrito');
+
+  const { status, data } = useFirestoreDocData(burritoRef);
+
+  if (status === "loading") {
+    return <p>Fetching burrito flavor...</p>;
+  }
+
+  return <p>The burrito is liked: {data.liked}!</p>;
+
+}
+
 
 function App() {
+
   return (
     <div>
-      
+      <Burrito/>
     </div>
   );
 }
