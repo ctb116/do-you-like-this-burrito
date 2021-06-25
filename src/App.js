8d7864c56ab9aa3c31ca1bfd198927fea109ba3b
Cathy T Bradley <cathytbradley116@gmail.com>
2021-06-24 17:11:11 -0700

    add burrito image and App styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Burrito from './components/Burrito';
+import burritoImage from './assets/istockphoto-burrito.jpg'
 
 const App = () => {
   return (
-    <div>
+    <div style={{textAlign: 'center'}}>
+      <h1>Do you like this Burrito?</h1>
+      <img src={burritoImage}/>
       <Burrito/>
     </div>
   )
